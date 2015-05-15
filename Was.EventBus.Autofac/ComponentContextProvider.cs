e7fbe93f90d33b19716527208d762874bc0765b5
Patryk Wąsiewicz <pat.wasiewicz@gmail.com>
2015-05-15 11:33:13 +0200

    Additional files

---
+++ Was.EventBus.Autofac/ComponentContextProvider.cs
@@ -0,0 +1,40 @@
+﻿namespace OpenTeamProject.EventBus.AutofacIntegration
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Autofac;
+
+    public class ComponentContextProvider : IImplementationProvider
+    {
+        private readonly IComponentContext componentContext;
+        private readonly object keyedService;
+
+        public ComponentContextProvider(IComponentContext componentContext, object keyedService = null)
+        {
+            this.componentContext = componentContext;
+            this.keyedService = keyedService;
+        }
+
+        public IEnumerable<IEvent> For(Type eventType)
+        {
+            var allImplementationType = typeof(IEnumerable<>).MakeGenericType(eventType);
+
+            IEnumerable allImplementations;
+            if (keyedService != null)
+            {
+                allImplementations =
+                    (IEnumerable)this.componentContext.ResolveKeyed(this.keyedService, allImplementationType);
+            }
+            else
+            {
+                allImplementations =
+                    (IEnumerable)this.componentContext.Resolve(allImplementationType);
+
+            }
+
+            return allImplementations.Cast<IEvent>();
+        }
+    }
+}
---
+++ Was.EventBus.Autofac/EventBusModule.cs
@@ -0,0 +1,52 @@
+﻿namespace OpenTeamProject.EventBus.AutofacIntegration
+{
+    using Autofac;
+    using Invokers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Module = Autofac.Module;
+
+    public class EventBusModule : Module
+    {
+        private readonly Assembly[] assemblies;
+
+        public EventBusModule(Assembly[] assemblies)
+        {
+            this.assemblies = assemblies;
+        }
+
+        private IEnumerable<Type> GetAllEventTypes()
+        {
+            return
+                this.assemblies.SelectMany(
+                                           asm =>
+                                           asm.GetTypes()
+                                              .Where(type => (typeof(IEvent).IsAssignableFrom(type)))
+                                              .Where(type => type.IsInterface)
+                                              .Where(type => type != typeof(IEvent))).Distinct();
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            foreach (var eventType in this.GetAllEventTypes())
+            {
+                var localEventType = eventType;
+                var eventTypeKey = new object();
+
+                builder.RegisterAssemblyTypes(this.assemblies)
+                       .AssignableTo(localEventType)
+                       .As(localEventType)
+                       .InstancePerRequest()
+                       .Keyed(eventTypeKey, eventType);
+
+                builder.Register(ctx => MultiInterfaceProxy.For(localEventType,
+                                                                new ComponentContextProvider(ctx, eventTypeKey),
+                                                                resolveEventsImmediately: true))
+                       .As(localEventType)
+                       .InstancePerRequest();
+            }
+        }
+    }
+}
